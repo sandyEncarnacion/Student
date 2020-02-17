e85c6cb88a689a516bb7d67056b76bf697c1463e
sandy <sandy-encarnacion@live.com>
2020-02-17 07:56:20 -0400

    Add project files.

---
+++ StudentReg/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StudentReg.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
